fb17daa1b5932da249f22d9e7e8606e757743a75
steavengong <842228597@qq.com>
2016-05-13 13:17:45 +0800

    MZC

--- ComProject/WXProject/Meadjohnson/MZC20160504/js/controllers/starterCtrl.js
+++ ComProject/WXProject/Meadjohnson/MZC20160504/js/controllers/starterCtrl.js
@@ -2,14 +2,11 @@
  * Created by Administrator on 2016/5/4.
  */
 angular.module("controllers.starter",[])
-    .controller("starterCtrl",["$scope","$state","$config","$wx","$modal",function($scope,$state,$config,$wx,$modal){
+    .controller("starterCtrl",["$scope","$state","$config","$wx",function($scope,$state,$config,$wx){
 
         $scope.subscribe = 0;
         $wx.personInfo.openId = "osFl-s9U62wClHjqywSNyFIy-Inc";
         $state.go($config.controllers.home.name);
-        /*$modal.init($config.modals.rule);
-        $modal.init($config.modals.enjoy);
-        $modal.init($config.modals.prize);*/
 
         /*$wx.redirect().then(function(result){
             var response = result.response;
