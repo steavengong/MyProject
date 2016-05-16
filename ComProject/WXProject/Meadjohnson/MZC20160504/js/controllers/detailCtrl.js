d95fc216df4d68fa5787193be6105bcc709b770b
steavengong <842228597@qq.com>
2016-05-16 10:00:00 +0800

    MZC

--- ComProject/WXProject/Meadjohnson/MZC20160504/js/controllers/detailCtrl.js
+++ ComProject/WXProject/Meadjohnson/MZC20160504/js/controllers/detailCtrl.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2016/5/4.
  */
 angular.module("controllers.detail",[])
-    .controller("detailCtrl",["$scope","$state","$config","$wx","$stateParams","$modal","$httpServices",function($scope,$state,$config,$wx,$stateParams,$modal,$httpServices){
+    .controller("detailCtrl",["$scope","$state","$config","$wx","$stateParams","$modal","$httpServices","$alert",function($scope,$state,$config,$wx,$stateParams,$modal,$httpServices,$alert){
         findBabyDetail();
 
         function findBabyDetail(){
@@ -21,6 +21,47 @@ angular.module("controllers.detail",[])
 
         $scope.goHome = function(){
             $state.go($config.controllers.home.name);
+        }
+
+        $scope.checkOpenId = function(){
+            if($stateParams.openId == $config.personInfo.openId){
+                return true;
+            }
+            return false;
+        }
+
+        $scope.voteByBallot = function($event){
+            $event.stopPropagation();
+            if($config.personInfo.subscribe==0){
+                $alert.show("请先关注")
+            }
+            else{
+                voteByBallot()
+            }
+        }
+
+        function voteByBallot(){
+            var action = $config.getRequestAction();
+            var data = {
+                "cmd" : $config.cmds.voteByBallot,
+                "parameters" : {
+                    "openId" : $config.personInfo.openId,
+                    "passiveOpenId":$stateParams.openId
+                }
+            };
+
+            $httpServices.getJsonFromPost(action,data)
+                .then(function(result){
+                    var response = result.response;
+                    $alert.show(response.flag);
+                })
+        }
+
+        $scope.checkIsJoin = function(){
+            if($config.personInfo.isJoin){
+                return true;
+            }
+            return false;
         }
 
     }])
--- ComProject/WXProject/Meadjohnson/MZC20160504/js/controllers/rankingListCtrl.js
+++ ComProject/WXProject/Meadjohnson/MZC20160504/js/controllers/rankingListCtrl.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2016/5/6.
  */
 angular.module("controllers.rankingList",[])
-    .controller("rankingListCtrl",["$scope","$ionicScrollDelegate","$location","$ionicHistory",function($scope,$ionicScrollDelegate,$location,$ionicHistory){
+    .controller("rankingListCtrl",["$scope","$ionicScrollDelegate","$location","$ionicHistory","$httpServices","$config","$modal",function($scope,$ionicScrollDelegate,$location,$ionicHistory,$httpServices,$config,$modal){
 
         $scope.backToTop = function(hashId){
             if ($location.hash() !== hashId) {
@@ -14,5 +14,57 @@ angular.module("controllers.rankingList",[])
         $scope.back = function(){
             $ionicHistory.goBack();
         }
+
+        findBabyDetail();
+
+        function findBabyDetail(){
+            var action = $config.getRequestAction();
+            var data = {
+                "cmd" : $config.cmds.findBabyDetail,
+                "parameters" : {
+                    "openId" : $config.personInfo.openId,
+                }
+            };
+            $httpServices.getJsonFromPost(action,data)
+                .then(function(result){
+                    $scope.detailObj = result.response;
+                    console.log($scope.detailObj);
+                })
+        }
+
+        $scope.rankItems = [];
+        var itemRankTotal = 0;
+        var itemPerPage = 30;
+        searchRanking();
+        function searchRanking(){
+
+            var action = $config.getRequestAction();
+            var data = {
+                "cmd" : $config.cmds.searchName,
+                "parameters" : {
+                    "ranking" : 1
+                }
+            };
+
+            $httpServices.getJsonFromPost(action,data)
+                .then(function(result){
+                    if(result.response){
+                        var responseData = result.response.data;
+                        if(responseData.rows.length>0){
+                            itemRankTotal = responseData.records;
+                            addRankItem(responseData.rows);
+                        }
+                    }
+                })
+        }
+
+        function addRankItem(items){
+            for(var item in items){
+                $scope.rankItems.push(items[item]);
+            }
+            console.log($scope.rankItems);
+        }
+
+
 
     }])
