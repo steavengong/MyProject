c3b25f25255e0321c921263091ca375896c25d53
steavengong <842228597@qq.com>
2016-05-20 16:05:26 +0800

    MZC

--- ComProject/WXProject/Meadjohnson/MZC20160504/js/providers/config.js
+++ ComProject/WXProject/Meadjohnson/MZC20160504/js/providers/config.js
@@ -11,13 +11,13 @@ angular.module("providers.config",[])
 
         $config.requestAction = "http://weixin.mengbp.com/wine-weixin-rest/cgi";
 
-        //$config.requestActionDebug = "http://192.168.100.228:8081/wine-weixin-rest/cgi";
-        $config.requestActionDebug = "http://erpuat.mengbp.com:8090/wine-weixin-rest/cgi";
+        $config.requestActionDebug = "http://192.168.100.228:8081/wine-weixin-rest/cgi";
+        //$config.requestActionDebug = "http://erpuat.mengbp.com:8090/wine-weixin-rest/cgi";
 
         $config.requestEnjoyAction = "http://weixin.mengbp.com/wine-weixin-rest/mzc/weixin/attendMzc";
 
-        //$config.requestEnjoyActionDebug = "http://192.168.100.228:8081/wine-weixin-rest/mzc/weixin/attendMzc";
-        $config.requestEnjoyActionDebug = "http://erpuat.mengbp.com:8090/wine-weixin-rest/mzc/weixin/attendMzc";
+        $config.requestEnjoyActionDebug = "http://192.168.100.228:8081/wine-weixin-rest/mzc/weixin/attendMzc";
+        //$config.requestEnjoyActionDebug = "http://erpuat.mengbp.com:8090/wine-weixin-rest/mzc/weixin/attendMzc";
 
         $config.appId = "wx5bb398c959489ae4";
 
@@ -45,7 +45,7 @@ angular.module("providers.config",[])
 
         $config.personInfo = {
             subscribe:1,
-            isJoin:0,
+            isJoin:1,
             openId:"osFl-s9U62wClHjqywSNyFIy-Inc",
             status:0,
             isDeadline:1
