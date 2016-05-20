00a008d331d7d33af5c75d3b5bdee0e28fa10130
steavengong <842228597@qq.com>
2016-05-20 15:00:49 +0800

    MZC

--- ComProject/WXProject/Meadjohnson/MZC20160504/js/providers/config.js
+++ ComProject/WXProject/Meadjohnson/MZC20160504/js/providers/config.js
@@ -45,7 +45,7 @@ angular.module("providers.config",[])
 
         $config.personInfo = {
             subscribe:1,
-            isJoin:1,
+            isJoin:0,
             openId:"osFl-s9U62wClHjqywSNyFIy-Inc",
             status:0,
             isDeadline:1
