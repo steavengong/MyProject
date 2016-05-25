4046c09f9f51d8a8756c8a20303eb1608dd31096
steavengong <842228597@qq.com>
2016-05-25 14:40:13 +0800

    MZC

---
+++ ComProject/AppProject/mbpz/js/tabs/tabs.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2016/5/25.
+ */
+(function(){
+    mui.init({
+        subpages:[{
+            id:"tabs-home",
+            url:"templates/"
+        }]
+    })
+
+
+
+
+
+})();
